c83965b528d34ad9b1ee47f8d9b2ca0760599602
Andrew Vuong <andrewtvuong@utexas.edu>
2019-05-01 22:19:26 -0500

    bug fixes! and cleaned up code

--- PIR/tweetpir.ino
+++ PIR/tweetpir.ino
@@ -16,49 +16,42 @@ int pirState = false;            // we start, assuming no motion detected
 int val = 0;                     // volt read
 int volt_threshold = 500;
 
-//WiFi
-const char ssid[] = "your_SSID";
-const char xpsk[] = "your_psk";
-
-//IFTTT
-char ifttt[] = "maker.ifttt.com";
-String makerKey = "your_ifttt_webhook_maker_key";
-
 void setup() {
-  pinMode(inputPin, INPUT);     // declare sensor as input
-  Serial.begin(115200);
-  Serial.println("start");
-  WiFi.begin(ssid, xpsk);
-  WiFi.mode(WIFI_STA);
-  delay(3000);
+    pinMode(inputPin, INPUT);     // declare sensor as input
+    Serial.begin(115200);
+    Serial.println("start");
+    WiFi.hostname("largecheesepizza"); // name network device
+    WiFi.begin("router", "password"); // psk acceptable for passphrase
+    WiFi.mode(WIFI_STA);
+    delay(3000);
 }
 
 void loop() {
-  val = analogRead(inputPin); // 1024 if triggered, otherwise hovers around 15
-  if (val > volt_threshold) {            // check if the input is above threshold
-    if (!pirState) {
-      // we have just turned on
-      Serial.println("Motion detected!");      
-      delay(1000);
-      if ((WiFi.status() == WL_CONNECTED)) {
-        USE_SERIAL.println(WiFi.localIP());
-        HTTPClient http;
-        USE_SERIAL.print("Transmitting...\n");
-        http.begin("maker.ifttt.com", 80, "/trigger/connected/with/key/" + makerKey + "?value1=" + val); //HTTP
-        // JSON is weird. {"value1":"1028","value2":"512"} dictionary becomes ?value1=1024&value2=512
-        // int httpCode = http.GET(); // use return code if want to check response
-        http.end();
-        USE_SERIAL.println("Transmission completed.");
-        pirState = true; // blocks re-entry after successful transmission, and while continously true for motion detected
-      }
-    }    
-  } 
-  else {
-    if (pirState) {
-      // we have just turned of
-      Serial.println("Motion ended!");
-      pirState = false;
-      delay(30000); // no refractory period with PaPIR compared to standard PIR therefore need to impose sizeable delay. 30s
-    }
-  }  
+    val = analogRead(inputPin); // 1024 if triggered, otherwise hovers around 15  
+    if (val > volt_threshold) {            // check if the input is above threshold
+        if (!pirState) {
+            // we have just turned on
+            Serial.println("Motion detected!");
+            if ((WiFi.status() == WL_CONNECTED)) {
+                String finaltrigger = "/trigger/{event_name}/with/key/{maker_key}?value1=" + String(random(1000)); // randomint avoids duplicate status error
+                USE_SERIAL.println(WiFi.localIP());
+                HTTPClient http;
+                USE_SERIAL.print("Transmitting...\n");
+                http.begin("maker.ifttt.com", 80, finaltrigger); //HTTP    
+                int httpCode = http.GET(); // use return code if want to check response
+                http.end();
+                USE_SERIAL.println("Transmission completed.");
+                pirState = true; // blocks re-entry after successful transmission, and while continously true for motion detected
+            }
+            delay(2000);
+        }    
+    } 
+    else {
+        if (pirState) {
+            // we have just turned of
+            Serial.println("Motion ended!");
+            pirState = false;
+            delay(3000);
+        }
+    }  
 }
